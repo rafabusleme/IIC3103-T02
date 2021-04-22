6740ea535d95ce7cfa92f67ceda36c75dcb99016
rafabusleme <raabusleme@uc.cl>
2021-04-22 17:13:22 -0400

    :rocket: create Track model

---
+++ src/migrations/20210422173531-create_tracks.js
@@ -0,0 +1,28 @@
+"use strict";
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable("Tracks", {
+      id: {
+        type: Sequelize.STRING,
+        primaryKey: true,
+        unique: true,
+      },
+      name: Sequelize.STRING,
+      duration: Sequelize.FLOAT,
+      timesPlayed: Sequelize.INTEGER,
+      albumId: {
+        type: Sequelize.STRING,
+        references: {
+          model: "Albumes",
+          key: "id",
+        },
+        onDelete: "CASCADE",
+      },
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable("Tracks");
+  },
+};
---
+++ src/models/track.js
@@ -0,0 +1,37 @@
+const { Model } = require("sequelize");
+
+module.exports = (sequelize, DataTypes) => {
+  class Track extends Model {
+    static associate(models) {
+      Track.belongsTo(models.Album, {
+        foreignKey: "albumId",
+        onDelete: "CASCADE",
+      });
+    }
+  }
+  Track.init(
+    {
+      id: {
+        type: DataTypes.STRING,
+        primaryKey: true,
+        unique: true,
+      },
+      name: DataTypes.STRING,
+      duration: DataTypes.FLOAT,
+      timesPlayed: DataTypes.INTEGER,
+      albumId: {
+        type: DataTypes.INTEGER,
+        references: {
+          model: "Album",
+          key: "id",
+        },
+      },
+    },
+    {
+      sequelize,
+      modelName: "Track",
+      timestamps: false,
+    }
+  );
+  return Track;
+};
